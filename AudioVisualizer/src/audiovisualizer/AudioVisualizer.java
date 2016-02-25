82ae295cb8fa13697353c0a4d1fb4970bb6c1e98
MatthiasSkopek <matthias.skopek@gmail.com>
2016-02-25 10:46:20 +0100

    CSS Heading

--- AudioVisualizer/src/audiovisualizer/AudioVisualizer.java
+++ AudioVisualizer/src/audiovisualizer/AudioVisualizer.java
@@ -10,7 +10,9 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -21,22 +23,27 @@ public class AudioVisualizer extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
+       Label mainLabel = new Label("Audio-Visualizer");
+       mainLabel.setId("Heading");
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(mainLabel);
         
         Scene scene = new Scene(root, 300, 250);
         
+        scene.getStylesheets().add("style.css");
         primaryStage.setTitle("AudioVisualizer");
+        
         primaryStage.setScene(scene);
         primaryStage.show();
     }
