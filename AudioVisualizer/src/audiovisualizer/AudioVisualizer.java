354aade18db4c88365efb261b41d6d75995643cf
Mattstir500 <stirmayr@gmail.com>
2016-02-25 09:41:46 +0100

    Titel
    Jo howi gendat

--- AudioVisualizer/src/audiovisualizer/AudioVisualizer.java
+++ AudioVisualizer/src/audiovisualizer/AudioVisualizer.java
@@ -36,7 +36,7 @@ public class AudioVisualizer extends Application {
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Audio Visualizer");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
