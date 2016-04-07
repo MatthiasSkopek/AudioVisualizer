886ce6daf075c27f7eee63a5d006fc2ce09f406d
Mattstir500 <stirmayr@gmail.com>
2016-04-07 10:36:35 +0200

    Little

--- AudioVisualizer/src/audiovisualizer/VUMeter.java
+++ AudioVisualizer/src/audiovisualizer/VUMeter.java
@@ -12,17 +12,40 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 /**
- * Simple 20bar meter
+ * 
  *
- * @author Jasper Potts
+ * @author Matthias Stirmayr
  */
 public class VUMeter extends Parent {
+
+    private int bildschirmbreite = 0;
+    private int am = 0;
+    Rectangle x;
+
+    public VUMeter(int bild, int ammount) {
+        this.bildschirmbreite = bild;
+        this.am = ammount;
+        x = new Rectangle((bildschirmbreite / am), 0);
+        colorList.add(Color.web("1abc9c"));
+        colorList.add(Color.web("f1c40f"));
+        colorList.add(Color.web("e67e22"));
+        colorList.add(Color.web("c0392b"));
+        colorList.add(Color.web("9b59b6"));
+        colorList.add(Color.web("3498db"));
+        colorList.add(Color.web("2c3e50"));
+        colorList.add(Color.web("f39c12"));
+        x.setStyle("bottom");
+        x.setFill(firstBAR_COLOR);
+        getChildren().addAll(x);
+        setEffect(DropShadowBuilder.create().blurType(BlurType.TWO_PASS_BOX).radius(10).spread(0.4).color(Color.web("#34495e")).build());
+        //setEffect(new MotionBlur(90, 8));
+    }
 
     private int colorIndex = 0;
     private final Color firstBAR_COLOR = Color.web("#cf0f0f");
     private ArrayList<Color> colorList = new ArrayList<>();
     //private Rectangle[] bars = new Rectangle[20];
-    Rectangle x = new Rectangle(10, 0);
+
     DoubleProperty value = new SimpleDoubleProperty(0) {
         @Override
         protected void invalidated() {
@@ -47,27 +70,14 @@ public class VUMeter extends Parent {
         return value;
     }
 
-    public VUMeter() {
-        colorList.add(Color.web("1abc9c"));
-       colorList.add(Color.web("f1c40f"));
-       colorList.add(Color.web("e67e22"));
-       colorList.add(Color.web("c0392b"));
-       colorList.add(Color.web("9b59b6"));
-       colorList.add(Color.web("3498db"));
-       colorList.add(Color.web("2c3e50"));
-       colorList.add(Color.web("f39c12"));
-        x.setStyle("bottom");
-        x.setFill(firstBAR_COLOR);
-        getChildren().addAll(x);
-        //setEffect(DropShadowBuilder.create().blurType(BlurType.TWO_PASS_BOX).radius(10).spread(0.4).color(Color.web("#b10000")).build());
-        //setEffect(new MotionBlur(90, 8));
-    }
-
     public void nextColor() {
         if (colorIndex + 2 == colorList.size()) {
             colorIndex = 0;
         }
         x.setFill(colorList.get(colorIndex));
         colorIndex++;
+    }
+    public void adjustwidth(int nwBildschirmwith){
+        x.setWidth(nwBildschirmwith/am);
     }
 }
